bf3cc1d54df9abc038e9508093249c044a1af6d9
Uday Pratap <udaypratap169@gmail.com>
2024-09-22 13:58:42 +0530

    c updated

---
+++ c/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int g(int p)
+{
+    printf("%d", p);
+    return p;
+}
+int h(int q)
+{
+    printf("%d", q);
+    return q;
+}
+void f(int x, int y)
+{
+    g(x);
+    h(y);
+}
+int main()
+{
+    f(g(10), h(20));
+}
